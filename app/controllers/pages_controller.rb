e1753612a832518cbb9336c8183264166590bab9
koileee <y872li@edu.uwaterloo.ca>
2017-05-06 13:56:03 -0400

    added login files

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  before_action :load_user
+
+  def index
+    @pages = Page.all
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  private
+
+  def load_user
+    @user = User.find(session[:current_user_id])
+  end
+end
